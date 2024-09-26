7e4d03064d440e9084884be187b6f7c67672195a
Steven Peng <pentwo@gmail.com>
2024-09-26 16:17:18 +0800

    MISC: Google converstion tag added STYLE: Fixing iframe height

--- src/components/general/Booking.tsx
+++ src/components/general/Booking.tsx
@@ -2,7 +2,7 @@
 
 import { Stack, Container, Loader, Group } from "@mantine/core";
 import { useViewportSize } from "@mantine/hooks";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 // NEXT IMPORTS
 
@@ -15,24 +15,48 @@ import React, { useState } from "react";
 // TYPE IMPORTS
 
 // FUNCTION IMPORTS
+import TagManager from "react-gtm-module";
 
 // STYLE IMPORTS
 
 interface Props {}
 
+const tagManagerArgs = {
+  gtmId: "AW-784873740",
+};
+
 const Booking = (props: Props) => {
   const { height, width } = useViewportSize();
   const [isLoading, setIsLoading] = useState(true);
 
+  const [iframeHeight, setIframeHeight] = useState(height - 320);
+
+  useEffect(() => {
+    // Adjust iframe height when window is resized
+    const handleResize = () => {
+      const newHeight = window.innerHeight - 320;
+      setIframeHeight(newHeight);
+    };
+
+    window.addEventListener("resize", handleResize);
+    handleResize(); // Initial call to set the correct height
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  useEffect(() => {
+    TagManager.initialize(tagManagerArgs);
+  }, []);
+
   return (
     <Container mt={60}>
-      <Stack>
+      <Stack style={{ height: "100%" }}>
         {/* <Group position="center">{isLoading && <Loader size={48} color="pink" />}</Group> */}
         <iframe
           //   src="https://squareup.com/appointments/book/2scr8j5ghv08kg/HGVQ3HMRPZ4E7/start"
           src="https://icare-remedial-massage.square.site/"
           //   width={width - 60}
-          height={height - 320}
+          height={iframeHeight}
           //   frameBorder={0}
           scrolling="auto"
           //   onLoad={() => setIsLoading(true)}
