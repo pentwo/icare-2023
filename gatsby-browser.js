f93fc257897673486b97f4c39a479785f35794d7
Steven Peng <pentwo@gmail.com>
2024-09-26 16:50:45 +0800

    MISC: gtag

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -9,3 +9,20 @@ exports.onRouteUpdate = ({ location }) => {
     });
   }
 };
+
+exports.onClientEntry = () => {
+  if (typeof window !== "undefined") {
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      window.dataLayer.push(arguments);
+    }
+    gtag("js", new Date());
+    gtag("config", "AW-784873740");
+
+    // Load the gtag.js script
+    const script = document.createElement("script");
+    script.src = "https://www.googletagmanager.com/gtag/js?id=AW-784873740";
+    script.async = true;
+    document.head.appendChild(script);
+  }
+};
