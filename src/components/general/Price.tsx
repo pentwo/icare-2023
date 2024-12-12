025afe24b084fba5d6287bc580dd46a4034aca25
Steven Peng <pentwo@gmail.com>
2024-12-12 10:33:54 +0800

    Update Price.tsx
    Update to new price 
    $280 initial
    $200 standard

--- src/components/general/Price.tsx
+++ src/components/general/Price.tsx
@@ -76,11 +76,11 @@ const priceInfo: PriceProps[] = [
     price: [
       {
         time: "Initial treatment (including consultation) 120 mins",
-        price: 250,
+        price: 280,
       },
       {
         time: "Follow up treatment 90 mins",
-        price: 170,
+        price: 200,
       },
     ],
   },
